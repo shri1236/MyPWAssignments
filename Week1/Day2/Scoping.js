8d40a349deaf019b41211061ccb9b6cb1e59365a
shri1236 <shri.somayaji@gmail.com>
2025-12-17 11:49:10 +0530

    Add files via upload
    Day1 Assignment

---
+++ Week1/Day2/Scoping.js
@@ -0,0 +1,14 @@
+var browserVersion=88.3
+{
+    var browserName="Chrome"
+    let browserVersion=88.5
+    const loadUrl="xyx.com"
+    
+
+    console.log("Browser name inside block "+browserName)
+    console.log("Browser version inside block "+browserVersion)
+    console.log("loadUrl inside the block "+loadUrl)
+}
+    //console.log("Browser name outside block "+browserName)
+    console.log("Browser version outside block "+browserVersion)
+    //console.log("loadUrl outside the block"+loadUrl)
