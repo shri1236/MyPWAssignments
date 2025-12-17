5b44453aac8e8af644cf2b657189177419307126
shri1236 <shri.somayaji@gmail.com>
2025-12-17 12:16:00 +0530

    Add files via upload
    Assignment For Statement

---
+++ Week1/Day2/Statement.js
@@ -0,0 +1,18 @@
+let mark=81
+
+if(mark>=90)
+{
+    console.log("A Grade")
+}
+else if(mark>=80)
+{
+    console.log("B Grade")
+}
+else if(mark>=70)
+{
+    console.log("C Grade")
+}
+else
+{
+ console.log("Fail")
+}
